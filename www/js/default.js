c1e83109ca49e5b49760d42f9469ef37e55823b6
m-sota <c0a20162e2@edu.teu.ac.jp>
2023-09-30 00:41:34 +0900

    add search

---
+++ www/js/default.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // 下からスライドインする要素と戻るボタンの要素を取得
+    const slideInContent = document.getElementById('slide-in-content');
+
+    // ボタンをクリックしたときのアクションを定義
+    document.getElementById('search-bar').addEventListener('click', () => {
+        slideInContent.style.transform = 'translateY(0%)'; // 下からスライドイン
+    });
+
+    // スワイプ（タッチ）イベントを処理するための変数
+    let touchStartY = 0;
+
+    // タッチ開始時のY座標を記録
+    document.addEventListener('touchstart', (e) => {
+        touchStartY = e.touches[0].clientY;
+    });
+
+    // タッチ終了時のY座標と開始時のY座標を比較して戻る処理を行う
+    document.addEventListener('touchend', (e) => {
+        const touchEndY = e.changedTouches[0].clientY;
+        const deltaY = touchEndY - touchStartY;
+
+        if (deltaY > 50) {
+            slideInContent.style.transform = 'translateY(100%)'; // 下にスワイプして戻る
+        }
+    });
+});
