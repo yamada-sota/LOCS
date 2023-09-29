ffccaa3c95142d49c6240d5925df4d4e812223d5
m-sota <c0a20162e2@edu.teu.ac.jp>
2023-09-29 21:29:04 +0900

    fix boot\; add default\(no map\)

--- www/js/boot.js
+++ www/js/boot.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
     var confirmButton = document.getElementById("confirm-button");
-    var transitionLink = document.getElementById("transition-link");
     
     // ニックネームが入力されている場合、確定ボタンを押すとフェードアウト＆インでページ遷移
     confirmButton.addEventListener("click", function () {
@@ -17,13 +16,33 @@ document.addEventListener("DOMContentLoaded", function () {
     });
     // ページ読み込み時にloadedクラスを追加
     document.body.classList.add("loaded");
+});
 
+document.addEventListener("DOMContentLoaded", function () {
+    var skipButton = document.getElementById("skip-button");
+    
     // スキップボタンを押すとフェードアウト＆インでページ遷移
-    transitionLink.addEventListener("click", function (e) {
-        e.preventDefault(); // リンクのデフォルトの動作をキャンセル
-        document.body.classList.add("page-transitioning");
-        setTimeout(function () {
-            window.location.href = transitionLink.href;
-        }, 250);
+    skipButton.addEventListener("click", function (e) {
+        e.preventDefault(); // リンクのデフォルト動作をキャンセル
+
+        var nicknameInput = document.querySelector(".nickname-input-field");
+        var nickname = nicknameInput.value.trim();
+        
+        if (nickname !== "") {
+            var confirmSkip = confirm("ニックネームが入力されています。本当にスキップしますか？");
+            if (confirmSkip) {
+                document.body.classList.add("page-transitioning");
+                setTimeout(function () {
+                    window.location.href = "default.html";
+                }, 250);
+            }
+        } else {
+            document.body.classList.add("page-transitioning");
+            setTimeout(function () {
+                window.location.href = "default.html";
+            }, 250);
+        }
     });
+    // ページ読み込み時にloadedクラスを追加
+    document.body.classList.add("loaded");
 });
