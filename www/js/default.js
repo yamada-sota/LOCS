50d982e616b878eaf2487c99b3dfd8fb6657354e
m-sota <c0a20162e2@edu.teu.ac.jp>
2023-10-02 00:01:27 +0900

    update search \(not yet budget, time\)

--- www/js/default.js
+++ www/js/default.js
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', function () {
         touchStartY = e.touches[0].clientY;
     });
 
-    // タッチ終了時のY座標と開始時のY座標を比較して戻る処理を行う
+    // タッチ終了時と開始時のY座標を比較
     document.addEventListener('touchend', (e) => {
         const touchEndY = e.changedTouches[0].clientY;
         const deltaY = touchEndY - touchStartY;
@@ -25,3 +25,19 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
+
+// 時間と分の要素を取得
+const hourSelect = document.getElementById('hour');
+const minuteSelect = document.getElementById('minute');
+
+// 時間が変更されたときの処理
+hourSelect.addEventListener('change', updateTime);
+minuteSelect.addEventListener('change', updateTime);
+
+function updateTime() {
+    const selectedHour = hourSelect.value;
+    const selectedMinute = minuteSelect.value;
+    
+    // 選択された時間と分を表示するなど、必要な処理を追加できます
+    console.log(`選択された時間: ${selectedHour}:${selectedMinute}`);
+}
