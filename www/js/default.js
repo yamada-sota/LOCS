65121a03e1dec06ff27323a225d8f3b067cc40d1
m-sota <c0a20162e2@edu.teu.ac.jp>
2023-10-02 22:40:18 +0900

    comp default\(add map\)\; update search\(add keywords\)\;

--- www/js/default.js
+++ www/js/default.js
@@ -24,6 +24,36 @@ document.addEventListener('DOMContentLoaded', function() {
             slideInContent.style.transform = 'translateY(100%)'; // 下にスワイプして戻る
         }
     });
+
+    // 初期マップ表示
+    let map;
+    function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: { lat: 0, lng: 0 }, // マップの初期位置
+            zoom: 15 // ズームレベル (1〜20)
+        });
+
+        // 現在地を表示
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                const userLocation = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                };
+                map.setCenter(userLocation);
+
+                new google.maps.Marker({
+                    position: userLocation,
+                    map: map,
+                    title: '現在位置'
+                });
+            });
+        } else {
+            alert('Geolocationがサポートされていません。');
+        }
+    }
+
+    initMap();
 });
 
 function redirectToSearchResult() {
