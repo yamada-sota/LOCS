c307dd9f7f258b52b36dc0d4a591f677005e6b5b
m-sota <c0a20162e2@edu.teu.ac.jp>
2023-10-05 17:27:46 +0900

    update boot\(input\)

--- www/js/boot.js
+++ www/js/boot.js
@@ -1,35 +1,68 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var confirmButton = document.getElementById("confirm-button");
-    
+document.addEventListener("DOMContentLoaded", function () {  
     // ニックネームが入力されている場合、確定ボタンを押すとフェードアウト＆インでページ遷移
-    confirmButton.addEventListener("click", function () {
-        var nicknameInput = document.querySelector(".nickname-input-field");
-        var nickname = nicknameInput.value.trim();
-        if (nickname === "") {
-            alert("ニックネームを入力してください。");
+    document.getElementById("confirm-button").addEventListener("click", function () {
+        var email = document.querySelector("#emailField").value;
+        var password = document.querySelector("#passwordField").value;
+        var confirmpassword = document.querySelector("#confirmpasswordField").value;
+        var later = document.querySelector(".later");
+        
+        if (email === "" || password === "" || confirmpassword === "") {
+            later.textContent = "未入力の欄があります。";
+            later.style.color = "var(--lighterror)";
+            return;
         } else {
-            document.body.classList.add("page-transitioning");
-            setTimeout(function () {
-                window.location.href = "defaultA.html";
-            }, 250);
+            if (password.length < 8) {
+                later.textContent = "パスワードは8文字以上で設定してください。";
+                later.style.color = "var(--lighterror)";
+                return;
+            } else if (password !== confirmpassword) {
+                later.textContent = "パスワードが一致しません。";
+                later.style.color = "var(--lighterror)";
+                return;
+            } else {
+                later.textContent = "登録しました。";
+                later.style.color = "var(--statussucess)";
+
+                // SQLite DBに接続
+                var db = window.sqlitePlugin.openDatabase({ name: 'my.db' });
+
+                // テーブルを作成
+                db.transaction(function(tx) {
+                    tx.executeSql('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, email TEXT, password TEXT)');
+                });
+
+                // DBに挿入
+                db.transaction(function(tx) {
+                    tx.executeSql('INSERT INTO users (email, password) VALUES (?, ?)', [email, password], function(tx, res) {
+                        document.body.classList.add("page-transitioning");
+                        setTimeout(function () {
+                            window.location.href = "defaultA.html";
+                        }, 250);
+                        // ページ読み込み時にloadedクラスを追加
+                        document.body.classList.add("loaded");
+                    }, function(error) {
+                        alert('登録に失敗しました: ' + error.message);
+                    });
+                });
+
+                
+            }
         }
     });
-    // ページ読み込み時にloadedクラスを追加
-    document.body.classList.add("loaded");
+    
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-    var skipButton = document.getElementById("skip-button");
-    
     // スキップボタンを押すとフェードアウト＆インでページ遷移
-    skipButton.addEventListener("click", function (e) {
+    document.getElementById("skip-button").addEventListener("click", function (e) {
         e.preventDefault(); // リンクのデフォルト動作をキャンセル
 
-        var nicknameInput = document.querySelector(".nickname-input-field");
-        var nickname = nicknameInput.value.trim();
+        var email = document.querySelector("#emailField").value;
+        var password = document.querySelector("#passwordField").value;
+        var confirmpassword = document.querySelector("#confirmpasswordField").value;
         
-        if (nickname !== "") {
-            var confirmSkip = confirm("ニックネームが入力されています。本当にスキップしますか？");
+        if (email !== "" || password !== "" || confirmpassword !== "") {
+            var confirmSkip = confirm("入力が途中です。スキップしますか？");
             if (confirmSkip) {
                 document.body.classList.add("page-transitioning");
                 setTimeout(function () {
@@ -46,8 +79,3 @@ document.addEventListener("DOMContentLoaded", function () {
     // ページ読み込み時にloadedクラスを追加
     document.body.classList.add("loaded");
 });
-
-screen.addEventListener("orientationchange", () => {
-  console.log(`The orientation of the screen is: ${screen.orientation}`);
-});
-
