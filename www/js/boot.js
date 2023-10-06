b1fb07c669c517b03819c1b4a26731f7682bd068
m-sota <c0a20162e2@edu.teu.ac.jp>
2023-10-07 04:06:57 +0900

    update boot\(update register, login, anonymous\)

--- www/js/boot.js
+++ www/js/boot.js
@@ -1,79 +1,131 @@
-document.addEventListener("DOMContentLoaded", function () {  
-    // ニックネームが入力されている場合、確定ボタンを押すとフェードアウト＆インでページ遷移
+document.addEventListener("DOMContentLoaded", function() {
+    var applicationkey   = "4dbb3d327127b2dec06b84deb304f3ce92a90fe3f413b160fbd7ad902ab74926";
+    var clientkey        = "dcd03e138124816b36a669ca227f97268de84662f737a23fc4a85aa2cd327d16"; 
+    var ncmb             = new NCMB(applicationkey, clientkey);
+    var registerOrLogin1 = document.getElementById("register-or-login-1");
+    var registerOrLogin2 = document.getElementById("register-or-login-2");
+    var userInput        = document.getElementById("user-input");
+    var userContainer    = document.querySelector(".user-container");
+    var skipButton       = document.getElementById("skip-button");
+    var later            = document.querySelector(".later");
+
+    registerOrLogin2.addEventListener("click", function () {
+        if (registerOrLogin1.textContent === "新規登録") {
+            registerOrLogin1.textContent = "ログイン";
+            registerOrLogin2.textContent = "新規登録画面へ";
+            userInput.innerHTML          = '<input type="email" id="emailField" class="user-field" placeholder="メールアドレス"><input type="password" style="display: block" minlength="8" id="passwordField" class="user-field" placeholder="パスワード">';
+            userContainer.style.bottom   = "20%";
+            skipButton.style.display     = "none";
+            later.textContent            = "";
+        } else if (registerOrLogin1.textContent === "ログイン") {
+            registerOrLogin1.textContent = "新規登録";
+            registerOrLogin2.textContent = "ログイン画面へ";
+            userInput.innerHTML          = '<input type="email" id="emailField" class="user-field" placeholder="メールアドレス">';
+            userContainer.style.bottom   = "25%";
+            skipButton.style.display     = "inline-flex";
+            later.textContent            = "あとから登録できます。";
+            later.style.color            = "var(--lightmain-80)";
+        }
+    });
+
     document.getElementById("confirm-button").addEventListener("click", function () {
         var email = document.querySelector("#emailField").value;
-        var password = document.querySelector("#passwordField").value;
-        var confirmpassword = document.querySelector("#confirmpasswordField").value;
-        var later = document.querySelector(".later");
         
-        if (email === "" || password === "" || confirmpassword === "") {
-            later.textContent = "未入力の欄があります。";
-            later.style.color = "var(--lighterror)";
-            return;
-        } else {
-            if (password.length < 8) {
-                later.textContent = "パスワードは8文字以上で設定してください。";
-                later.style.color = "var(--lighterror)";
-                return;
-            } else if (password !== confirmpassword) {
-                later.textContent = "パスワードが一致しません。";
+        if (registerOrLogin1.textContent === "新規登録") {
+            if (email === "") {
+                later.textContent = "メールアドレスが未入力です。";
                 later.style.color = "var(--lighterror)";
                 return;
             } else {
-                later.textContent = "登録しました。";
-                later.style.color = "var(--statussucess)";
-
-                // SQLite DBに接続
-                var db = window.sqlitePlugin.openDatabase({ name: 'my.db' });
-
-                // テーブルを作成
-                db.transaction(function(tx) {
-                    tx.executeSql('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, email TEXT, password TEXT)');
-                });
-
-                // DBに挿入
-                db.transaction(function(tx) {
-                    tx.executeSql('INSERT INTO users (email, password) VALUES (?, ?)', [email, password], function(tx, res) {
-                        document.body.classList.add("page-transitioning");
-                        setTimeout(function () {
-                            window.location.href = "defaultA.html";
-                        }, 250);
-                        // ページ読み込み時にloadedクラスを追加
-                        document.body.classList.add("loaded");
-                    }, function(error) {
-                        alert('登録に失敗しました: ' + error.message);
-                    });
-                });
-
-                
+                if (!email.includes("@")){
+                    later.innerHTML   = "メールアドレスの形式が<br>正しくありません。";
+                    later.style.color = "var(--lighterror)";
+                    return;
+                } else {
+                    // ユーザーデータをDBに保存
+                    ncmb.User.requestSignUpEmail(email)
+                        .then(function () {
+                            later.innerHTML   = "新規登録案内メールを<br>送信しました。";
+                            later.style.color = "var(--lightsuccess)";
+                            setTimeout(function () {
+                                registerOrLogin1.textContent = "ログイン";
+                                registerOrLogin2.textContent = "新規登録画面へ";
+                                userInput.innerHTML          = '<input type="email" id="emailField" class="user-field" placeholder="メールアドレス"><input type="password" style="display: block" minlength="8" id="passwordField" class="user-field" placeholder="パスワード">';
+                                userContainer.style.bottom   = "20%";
+                                skipButton.style.display     = "none";
+                                later.textContent            = "";
+                            }, 2000);
+                        })
+                        .catch(function (error) {
+                            later.innerHTML   = "登録に失敗しました。<br>エラーコード：" + error.code;
+                            later.style.color = "var(--lighterror)";
+                        });
+                }
+            }
+        } else if (registerOrLogin1.textContent === "ログイン") {
+            var password = document.querySelector("#passwordField").value;
+            if (email === "" || password === "") {
+                later.textContent = "未入力の欄があります。";
+                later.style.color = "var(--lighterror)";
+                return;
+            } else {
+                if (!email.includes("@")){
+                    later.innerHTML   = "メールアドレスの形式が<br>正しくありません。";
+                    later.style.color = "var(--lighterror)";
+                    return;
+                } else {
+                    // ログイン
+                    ncmb.User.loginWithMailAddress(email, password)
+                        .then(function () {
+                            later.textContent = "ログインに成功しました。";
+                            later.style.color = "var(--lightsuccess)";
+                            setTimeout(function () {
+                                document.body.classList.add("page-transitioning");
+                                setTimeout(function () {
+                                    window.location.href = "defaultA.html";
+                                }, 250);
+                                // ページ読み込み時にloadedクラスを追加
+                                document.body.classList.add("loaded");
+                            }, 800);
+                        })
+                        .catch(function (error) {
+                            later.innerHTML   = "ログインに失敗しました。<br>エラーコード：" + error.code;
+                            later.style.color = "var(--lighterror)";
+                        });
+                }
             }
         }
     });
-    
-});
 
-document.addEventListener("DOMContentLoaded", function () {
-    // スキップボタンを押すとフェードアウト＆インでページ遷移
     document.getElementById("skip-button").addEventListener("click", function (e) {
         e.preventDefault(); // リンクのデフォルト動作をキャンセル
-
         var email = document.querySelector("#emailField").value;
-        var password = document.querySelector("#passwordField").value;
-        var confirmpassword = document.querySelector("#confirmpasswordField").value;
         
-        if (email !== "" || password !== "" || confirmpassword !== "") {
+        if (email !== "") {
             var confirmSkip = confirm("入力が途中です。スキップしますか？");
             if (confirmSkip) {
-                document.body.classList.add("page-transitioning");
-                setTimeout(function () {
-                    window.location.href = "default.html";
-                }, 250);
+                ncmb.User.loginAsAnonymous()
+                    .then(function(){
+                        document.body.classList.add("page-transitioning");
+                        setTimeout(function () {
+                            window.location.href = "default.html";
+                        }, 250);
+                    })
+                    .catch(function(){
+                        // エラー処理
+                    });
             }
         } else {
-            document.body.classList.add("page-transitioning");
-            setTimeout(function () {
-                window.location.href = "default.html";
-            }, 250);
+            ncmb.User.loginAsAnonymous()
+                .then(function(){
+                    document.body.classList.add("page-transitioning");
+                    setTimeout(function () {
+                        window.location.href = "default.html";
+                    }, 250);
+                })
+                .catch(function(){
+                    // エラー処理
+                });
         }
     });
     // ページ読み込み時にloadedクラスを追加
