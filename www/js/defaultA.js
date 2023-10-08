23bf95fc3341bd2559d7705faeb85012011c31b0
m-sota <c0a20162e2@edu.teu.ac.jp>
2023-10-08 23:28:14 +0900

    update defaultA\(add userinfo, not mypage\)

--- www/js/defaultA.js
+++ www/js/defaultA.js
@@ -1,27 +1,37 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // 下からスライドインする要素と戻るボタンの要素を取得
+    // 下からスライドイン
     const slideInContent = document.getElementById("slide-in-content");
-
-    // ボタンをクリックしたときのアクションを定義
     document.getElementById("search-bar").addEventListener("click", () => {
-        slideInContent.style.transform = "translateY(0%)"; // 下からスライドイン
+        slideInContent.style.transform = "translateY(0%)";
     });
-
-    // スワイプ（タッチ）イベントを処理するための変数
     let touchStartY = 0;
-
-    // タッチ開始時のY座標を記録
     document.addEventListener("touchstart", (e) => {
         touchStartY = e.touches[0].clientY;
     });
-
-    // タッチ終了時と開始時のY座標を比較
     document.addEventListener("touchend", (e) => {
         const touchEndY = e.changedTouches[0].clientY;
         const deltaY    = touchEndY - touchStartY;
 
         if (deltaY > 50) {
-            slideInContent.style.transform = "translateY(100%)"; // 下にスワイプして戻る
+            slideInContent.style.transform = "translateY(100%)";
+        }
+    });
+
+    // 右からスライドイン
+    const slideInContent2 = document.getElementById("slide-in-content2");
+    document.getElementById("edit").addEventListener("click", () => {
+        slideInContent2.style.transform = "translateX(0%)";
+    });
+    let touchStartX = 0;
+    document.addEventListener("touchstart", (e) => {
+        touchStartX = e.touches[0].clientX;
+    });
+    document.addEventListener("touchend", (e) => {
+        const touchEndX = e.changedTouches[0].clientX;
+        const deltaX    = touchEndX - touchStartX;
+
+        if (deltaX > 50) {
+            slideInContent2.style.transform = "translateX(100%)";
         }
     });
 
