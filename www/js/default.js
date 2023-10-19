189b5f2072507950469b2c9f36c8781a7aad7bc5
m-sota <c0a20162e2@edu.teu.ac.jp>
2023-10-19 13:53:14 +0900

    add map.js\; fix bug\(404\)\;

--- www/js/default.js
+++ www/js/default.js
@@ -6,7 +6,6 @@ document.addEventListener("DOMContentLoaded", function() {
     var locsId         = document.getElementById("locs-id");
     var mypageIcon     = document.getElementById("mypage-icon-image");
     var icon           = document.getElementById("icon");
-    var avatar         = "https://mbaas.api.nifcloud.com/2013-09-01/applications/1er2zvbAsWIdFAEI/publicFiles/Avatar";
     
     var currentUser = new ncmb.User.getCurrentUser();
     if (currentUser) {
@@ -17,14 +16,15 @@ document.addEventListener("DOMContentLoaded", function() {
             icon.src       = dataUrl;
         }
         var fileName = currentUser.get("objectId");
-        ncmb.File.download(fileName, "blob")
-            .then(function(blob) {
-                reader.readAsDataURL(blob);
-            })
-            .catch(function() {
-                mypageIcon.src = avatar;
-                icon.src       = avatar;
-            })
+        var anonymous = (authData = currentUser.get("authData")) && authData["anonymous"] && !currentUser.get("password");
+        if (!anonymous) {
+            ncmb.File.download(fileName, "blob")
+                .then(function(blob) {
+                    reader.readAsDataURL(blob);
+                })
+                .catch(function() {
+                })
+        }
         userName.textContent = currentUser.get("userName");
         ncmb.User.fetchById(currentUser.get("objectId"))
             .then(function(user){
@@ -36,33 +36,6 @@ document.addEventListener("DOMContentLoaded", function() {
                 window.location.href = "index.html";
             });
     }
-
-    // 初期マップ表示
-    let map;
-    function initMap() {
-        map = new google.maps.Map(document.getElementById("map"), {
-            center: { lat: 0, lng: 0 }, // マップの初期位置
-            zoom: 15 // ズームレベル (1〜20)
-        });
-        // 現在地を表示
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(function (position) {
-                var userLocation = {
-                    lat: position.coords.latitude,
-                    lng: position.coords.longitude
-                };
-                map.setCenter(userLocation);
-                new google.maps.Marker({
-                    position: userLocation,
-                    map: map,
-                    title: "現在位置"
-                });
-            });
-        } else {
-            alert("Geolocationがサポートされていません。");
-        }
-    }
-    initMap();
 
     // 下からスライドイン
     var slideInContent = document.getElementById("slide-in-content");
