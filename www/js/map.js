279814aad962f6aa2a8cfede27dcc64c77d52bf1
m-sota <c0a20162e2@edu.teu.ac.jp>
2023-10-23 00:05:08 +0900

    add keywords

--- www/js/map.js
+++ www/js/map.js
@@ -4,8 +4,10 @@ var iconRotation = 0;
 function initMap() {
     var mapOptions = {
         zoom: 15,
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        // mapTypeId: google.maps.MapTypeId.ROADMAP,
+        mapId: "2e1aed6c4c23f06a",
         disableDefaultUI: true,
+        isFractionalZoomEnabled: true,
     };
     map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
@@ -27,15 +29,7 @@ function initMap() {
             // 回転角度
             var newIconRotation = position.coords.heading || 0;
 
-            // var customIcon = {
-            //     path: "M-1.547 12l6.563-6.609-1.406-1.406-5.156 5.203-2.063-2.109-1.406 1.406zM0 0q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z",
-            //     fillColor: "blue",
-            //     fillOpacity: 0.6,
-            //     strokeWeight: 0,
-            //     rotation: 0,
-            //     scale: 2,
-            //     anchor: new google.maps.Point(0, 20),
-            // };
+            // アイコンを作成
             var customIcon = {
                 path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
                 strokeColor: "rgba(156, 124, 69, 1)",
@@ -110,6 +104,10 @@ function searchLocation() {
                 position: location,
             });
             slideInContent.style.transform = "translateY(100%)";
+
+            console.log('検索結果:', results);
+            var formattedAddress = results[0].formatted_address;
+            console.log('フォーマットされた住所:', formattedAddress);
         } else {
             alert("場所が見つかりません。");
         }
