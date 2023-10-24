2fec2c0eec01d27cb2a1aa98cd7e3bbee1f34d69
m-sota <c0a20162e2@edu.teu.ac.jp>
2023-10-25 00:32:32 +0900

    feature departure, keyword, sort\(not price\)\; not budget, time, transportation\;

--- www/js/map.js
+++ www/js/map.js
@@ -1,4 +1,4 @@
-var map, watchID, marker, circle;
+var map, watchID, currentMarker, pinMarker, circle;
 var iconRotation = 0;
 
 function initMap() {
@@ -19,8 +19,8 @@ function initMap() {
             );
 
             // 以前のマーカーとサークルを削除
-            if (marker) {
-                marker.setMap(null);
+            if (currentMarker) {
+                currentMarker.setMap(null);
             }
             if (circle) {
                 circle.setMap(null);
@@ -39,12 +39,12 @@ function initMap() {
             };
 
             // マーカーを作成
-            marker = new google.maps.Marker({
+            currentMarker = new google.maps.Marker({
                 position: latLng,
                 map: map,
                 icon: customIcon,
             });
-            var iconDiv = marker.getIcon().anchor;
+            var iconDiv = currentMarker.getIcon().anchor;
             if (iconDiv) {
                 iconDiv.style.transform = "rotate(" + newIconRotation + "deg)";
             }
@@ -93,17 +93,17 @@ document.addEventListener("DOMContentLoaded", function() {
 var waitingForPin = true;
 var myLatLng      = 0;
 function handlePinPlacement() {
-    if (marker) {
-        marker.setMap(null);
+    if (pinMarker) {
+        pinMarker.setMap(null);
     }
 
     // マップ上でクリックした位置にピンを設置
     google.maps.event.addListener(map, "click", function (event) {
-        if (marker) {
-            marker.setMap(null);
+        if (pinMarker) {
+            pinMarker.setMap(null);
         }
 
-        marker = new google.maps.Marker({
+        pinMarker = new google.maps.Marker({
             position: event.latLng,
             map: map,
             icon: "https://mbaas.api.nifcloud.com/2013-09-01/applications/1er2zvbAsWIdFAEI/publicFiles/Pin",
@@ -115,12 +115,10 @@ function handlePinPlacement() {
 }
 
 function searchLocation() {
-    var currentLocationButton           = document.getElementById("current-location-button");
-    var confirmPinButton                = document.getElementById("confirm-pin-button");
-    var placesService                   = new google.maps.places.PlacesService(map);
-    var generatePlan                    = document.getElementById("generate-plan");
-    currentLocationButton.style.display = "none";
-    generatePlan.style.display          = "flex";
+    var currentLocationButton = document.getElementById("current-location-button");
+    var confirmPinButton      = document.getElementById("confirm-pin-button");
+    var placesService         = new google.maps.places.PlacesService(map);
+    var generatePlan          = document.getElementById("generate-plan");
 
     var departure = document.getElementById("departure").value;
     var keyword1  = document.getElementById("keywords1").value;
@@ -136,6 +134,28 @@ function searchLocation() {
     var bicycle   = document.getElementById("bicycle").checked;
     var sort      = document.getElementById("sort").value;
 
+    // 移動手段による検索半径
+    var radius = 0;
+    if (foot) {
+        radius += 1000;
+    }
+    if (train) {
+        radius += 1000;
+    }
+    if (car) {
+        radius += 1000;
+    }
+    if (bicycle) {
+        radius += 1000;
+    }
+    if (radius === 0) {
+        alert("移動手段を選択してください。");
+        return;
+    }
+
+    currentLocationButton.style.display = "none";
+    generatePlan.style.display          = "flex";
+
     // 差分計算
     // var startTime = new Date("2023-10-02T" + timePicker1);
     // var endTime   = new Date("2023-10-02T" + timePicker2);
@@ -146,8 +166,8 @@ function searchLocation() {
     // }
 
     navigator.geolocation.clearWatch(watchID);
-    if (marker) {
-        marker.setMap(null);
+    if (pinMarker) {
+        pinMarker.setMap(null);
     }
     if (circle) {
         circle.setMap(null);
@@ -171,44 +191,81 @@ function searchLocation() {
         confirmPinButton.style.display = "flex";
 
         handlePinPlacement();
-    } else {
-        // 非同期処理を制御するためのPromiseを返す関数
-        function geocodeKeyword(keyword) {
-            return new Promise((resolve, reject) => {
-                // Places APIで検索
-                var request = {
-                    query: keyword,
-                    fields: ["name", "geometry"]
+    } else if (departure === "current-loc") {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                const latLng = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude,
                 };
-                placesService.findPlaceFromQuery(request, (results, status) => {
-                    if (status === google.maps.places.PlacesServiceStatus.OK) {
-                        var placeLocation = results[0].geometry.location;
-                        map.setCenter(placeLocation);
-                        marker = new google.maps.Marker({
-                            map: map,
-                            position: placeLocation,
-                        });
-                        resolve(results);
-                    } else {
-                        reject("場所が見つかりません：" + keyword);
-                    }
-                });
-            });
-        }
+                // 非同期処理を制御するためのPromiseを返す関数
+                function geocodeKeyword(keyword) {
+                    return new Promise((resolve, reject) => {
+                        // Places APIで検索
+                        var request = {
+                            location: latLng,
+                            radius: radius,
+                            query: keyword,
+                        };
+                        placesService.textSearch(request, (results, status) => {
+                            if (status === google.maps.places.PlacesServiceStatus.OK) {
+                                if (sort === "distance") {
+                                    for (var i = 0; i < results.length; i++) {
+                                        var placeLocation = results[i].geometry.location;
+                                        if (i === 0) {
+                                            map.setCenter(placeLocation);
+                                            pinMarker = new google.maps.Marker({
+                                                map: map,
+                                                position: placeLocation,
+                                            });
+                                        }
+                                        resolve(results);
+                                    }
+                                } else if (sort === "rate") {
+                                    var maxRating      = -1;
+                                    var maxRatingIndex = -1;
+                                    for (let i = 0; i < results.length; i++) {
+                                        console.log(i + "：", results[i].rating);
+                                        if (results[i].rating > maxRating) {
+                                            maxRating      = results[i].rating;
+                                            maxRatingIndex = i;
+                                        }
+                                    }
+                                    var placeLocation = results[maxRatingIndex].geometry.location;
+                                    map.setCenter(placeLocation);
+                                    pinMarker = new google.maps.Marker({
+                                        map: map,
+                                        position: placeLocation,
+                                    });
+                                    resolve(results);
+                                    console.log("maxRatingIndex：", maxRatingIndex);
+                                    console.log("maxRatingResult：", results[maxRatingIndex]);
+                                } else if (sort === "price") {
 
-        // Promiseを順番に実行
-        (async () => {
-            for (let i = 0; i < keywords.length; i++) {
-                try {
-                    const results = await geocodeKeyword(keywords[i]);
-                    console.log(i + 1 + "件目：", results[0]);
-                } catch (error) {
-                    console.error(error);
+                                }
+                            } else {
+                                reject("場所が見つかりません：" + keyword);
+                            }
+                        });
+                    });
                 }
+
+                // Promiseを順番に実行
+                (async () => {
+                    for (let i = 0; i < keywords.length; i++) {
+                        try {
+                            const results = await geocodeKeyword(keywords[i]);
+                            console.log("キーワード" + (i + 1) + "：", results);
+                        } catch (error) {
+                            console.error(error);
+                        }
+                    }
+
+                    generatePlan.style.display     = "none";
+                    slideInContent.style.transform = "translateY(100%)";
+                })();
             }
-            generatePlan.style.display     = "none";
-            slideInContent.style.transform = "translateY(100%)";
-        })();
+        );
     }
 
     confirmPinButton.addEventListener("click", function() {
@@ -227,18 +284,45 @@ function searchLocation() {
                 // Places APIで検索
                 var request = {
                     location: myLatLng,
+                    radius: radius,
                     query: keyword,
-                    fields: ["name", "geometry"]
                 };
                 placesService.textSearch(request, (results, status) => {
                     if (status === google.maps.places.PlacesServiceStatus.OK) {
-                        var placeLocation = results[0].geometry.location;
-                        map.setCenter(placeLocation);
-                        marker = new google.maps.Marker({
-                            map: map,
-                            position: placeLocation,
-                        });
-                        resolve(results);
+                        if (sort === "distance") {
+                            for (var i = 0; i < results.length; i++) {
+                                var placeLocation = results[i].geometry.location;
+                                if (i === 0) {
+                                    map.setCenter(placeLocation);
+                                    pinMarker = new google.maps.Marker({
+                                        map: map,
+                                        position: placeLocation,
+                                    });
+                                }
+                                resolve(results);
+                            }
+                        } else if (sort === "rate") {
+                            var maxRating      = -1;
+                            var maxRatingIndex = -1;
+                            for (let i = 0; i < results.length; i++) {
+                                console.log(i + "：", results[i].rating);
+                                if (results[i].rating > maxRating) {
+                                    maxRating      = results[i].rating;
+                                    maxRatingIndex = i;
+                                }
+                            }
+                            var placeLocation = results[maxRatingIndex].geometry.location;
+                            map.setCenter(placeLocation);
+                            pinMarker = new google.maps.Marker({
+                                map: map,
+                                position: placeLocation,
+                            });
+                            resolve(results);
+                            console.log("maxRatingIndex：", maxRatingIndex);
+                            console.log("maxRatingResult：", results[maxRatingIndex]);
+                        } else if (sort === "price") {
+
+                        }
                     } else {
                         reject("場所が見つかりません：" + keyword);
                     }
@@ -251,13 +335,84 @@ function searchLocation() {
             for (let i = 0; i < keywords.length; i++) {
                 try {
                     const results = await geocodeKeyword(keywords[i]);
-                    console.log(i + 1 + "件目：", results[0]);
+                    console.log("キーワード" + (i + 1) + "：", results);
                 } catch (error) {
                     console.error(error);
                 }
             }
+            
             generatePlan.style.display     = "none";
             slideInContent.style.transform = "translateY(100%)";
         })();
     });
+
+    // 位置情報の監視を開始
+    watchID = navigator.geolocation.watchPosition(
+        function (position) {
+            var latLng = new google.maps.LatLng(
+                position.coords.latitude,
+                position.coords.longitude,
+            );
+
+            // 以前のマーカーとサークルを削除
+            if (currentMarker) {
+                currentMarker.setMap(null);
+            }
+            if (circle) {
+                circle.setMap(null);
+            }
+
+            // 回転角度
+            var newIconRotation = position.coords.heading || 0;
+
+            // アイコンを作成
+            var customIcon = {
+                path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
+                strokeColor: "rgba(156, 124, 69, 1)",
+                strokeWeight: 3,
+                scale: 5,
+                rotation: newIconRotation,
+            };
+
+            // マーカーを作成
+            currentMarker = new google.maps.Marker({
+                position: latLng,
+                map: map,
+                icon: customIcon,
+            });
+            var iconDiv = currentMarker.getIcon().anchor;
+            if (iconDiv) {
+                iconDiv.style.transform = "rotate(" + newIconRotation + "deg)";
+            }
+
+            // サークルを作成
+            circle = new google.maps.Circle({
+                strokeColor: "rgba(193, 161, 78, 1)",
+                strokeOpacity: 0.8,
+                strokeWeight: 1,
+                fillColor: "rgba(215, 182, 83, 1)",
+                fillOpacity: 0.1,
+                map: map,
+                draggable: false,
+                center: latLng,
+                radius: position.coords.accuracy, // 誤差の単位：m
+            });
+
+            // マップを中心に移動
+            map.setCenter(latLng);
+            // マップを回転
+            map.setHeading(newIconRotation);
+
+            // 回転角度の更新
+            iconRotation = newIconRotation;
+        },
+        function () {
+            alert("位置情報の取得に失敗しました");
+        },
+        {
+            enableHighAccuracy: true,
+            maximumAge: 0,
+            timeout: 5000,
+        },
+    );
 }
